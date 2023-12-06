6ccae4320287090abc80646090f1164149432a2e
Aminah Memon <aminah.memon@hertford.ox.ac.uk>
2023-12-06 22:27:43 +0000

    updated with brownian motion

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -4,21 +4,22 @@
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
+#brownian motion
+brownian_motion  <- function (n_particles, h = 0.25) {
   
-  df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  df <- data.frame(x = rep(NA, n_particles), y = rep(NA, n_particles), time = 1:n_particles)
   
   df[1,] <- c(0,0,1)
   
-  for (i in 2:n_steps) {
-    
-    h <- 0.25
+  for (i in 2:n_particles) {
+    #setting the standard deviation for the normal distribution as normal distribution (sigma) is a fundamental property of the displacements of a particle in Brownian motion
+    sigma <- h * sqrt(i-1) #setting h to be proportional to sigma by multiplying h by a scaling factor (sqrt of the time step)
     
     angle <- runif(1, min = 0, max = 2*pi)
     
-    df[i,1] <- df[i-1,1] + cos(angle)*h
+    df[i,1] <- df[i-1,1] + cos(angle)*sigma
     
-    df[i,2] <- df[i-1,2] + sin(angle)*h
+    df[i,2] <- df[i-1,2] + sin(angle)*sigma
     
     df[i,3] <- i
     
@@ -28,7 +29,9 @@ random_walk  <- function (n_steps) {
   
 }
 
-data1 <- random_walk(500)
+set.seed(123) # Setting a seed for reproducibility
+
+data1 <- brownian_motion(1000)
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
@@ -40,8 +43,8 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
 
+data2 <- brownian_motion(1000)
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
   geom_path(aes(colour = time)) +
@@ -53,3 +56,5 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   ylab("y-coordinate")
 
 grid.arrange(plot1, plot2, ncol=2)
+
+
